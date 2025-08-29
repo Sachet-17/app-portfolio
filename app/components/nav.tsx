7ca586a097446d6f859cce106b16971a7addff08
Sachet-17 <90276515+Sachet-17@users.noreply.github.com>
2025-08-29 12:16:14 -0400

    navigations

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,53 +3,61 @@ import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
+const links = [
+  { name: "About", href: "/about" },
+  { name: "Skills", href: "/skills" },
+  { name: "Education", href: "/education" },
+  { name: "Experience", href: "/experience" },
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+];
+
 export const Navigation: React.FC = () => {
-	const ref = useRef<HTMLElement>(null);
-	const [isIntersecting, setIntersecting] = useState(true);
+  const ref = useRef<HTMLElement>(null);
+  const [isIntersecting, setIntersecting] = useState(true);
 
-	useEffect(() => {
-		if (!ref.current) return;
-		const observer = new IntersectionObserver(([entry]) =>
-			setIntersecting(entry.isIntersecting),
-		);
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting)
+    );
 
-		observer.observe(ref.current);
-		return () => observer.disconnect();
-	}, []);
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
 
-	return (
-		<header ref={ref}>
-			<div
-				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
-					isIntersecting
-						? "bg-zinc-900/0 border-transparent"
-						: "bg-zinc-900/500  border-zinc-800 "
-				}`}
-			>
-				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
-					<div className="flex justify-between gap-8">
-						<Link
-							href="/projects"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
-						>
-							Projects
-						</Link>
-						<Link
-							href="/contact"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
-						>
-							Contact
-						</Link>
-					</div>
+  return (
+    <header ref={ref}>
+      <div
+        className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b ${
+          isIntersecting
+            ? "bg-zinc-900/0 border-transparent"
+            : "bg-zinc-900/50 border-zinc-800"
+        }`}
+      >
+        <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
+          {/* Right side nav links */}
+          <div className="flex justify-between gap-8">
+            {links.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className="duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
 
-					<Link
-						href="/"
-						className="duration-200 text-zinc-300 hover:text-zinc-100"
-					>
-						<ArrowLeft className="w-6 h-6 " />
-					</Link>
-				</div>
-			</div>
-		</header>
-	);
+          {/* Left side back arrow */}
+          <Link
+            href="/"
+            className="duration-200 text-zinc-300 hover:text-zinc-100"
+          >
+            <ArrowLeft className="w-6 h-6" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
