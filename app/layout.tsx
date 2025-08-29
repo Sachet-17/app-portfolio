98000cad6b15468a4f8d516f7500b6dc67b88e19
Sachet-17 <90276515+Sachet-17@users.noreply.github.com>
2025-08-28 21:08:46 -0400

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
 import "../global.css";
 import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Sachet Bisi — Portfolio",
+    template: "%s | Sachet Bisi",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Software Developer & AI/ML Engineer",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "Sachet Bisi — Portfolio",
+    description: "Software Developer & AI/ML Engineer",
+    url: "https://sachet-portfolio-views.vercel.app",
+    siteName: "Sachet Bisi",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "/og.png", // you can add your own OpenGraph image under /public/og.png
         width: 1920,
         height: 1080,
       },
@@ -38,13 +37,14 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Sachet Bisi — Portfolio",
     card: "summary_large_image",
   },
   icons: {
     shortcut: "/favicon.png",
   },
 };
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -66,8 +66,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
