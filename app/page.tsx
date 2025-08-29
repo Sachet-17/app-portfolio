9adb9e5fe6e54613a5fb3d484ea3cfd2474b53d5
Sachet-17 <90276515+Sachet-17@users.noreply.github.com>
2025-08-28 21:33:56 -0400

    n home

--- app/page.tsx
+++ app/page.tsx
@@ -33,13 +33,14 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        Sachet Ranjan Bisi
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I’m a Computer Engineer with a love for AI/ML, always chasing ideas that can turn into something useful in the real world. I enjoy building projects that make a difference—and I’m on the lookout for a full-time role where I can keep learning, creating, and solving meaningful problems.{" "}
+          I’m a Computer Engineer with a love for AI/ML, always chasing ideas that can turn into something useful in the real world. 
+          I enjoy building projects that make a difference—and I’m on the lookout for a full-time role where I can keep learning, creating, and solving meaningful problems.{" "}
         </h2>
       </div>
     </div>
