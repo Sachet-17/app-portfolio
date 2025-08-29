fef326d42d6927d7eb7ca4ecc68a692ae7ccc52f
Sachet-17 <90276515+Sachet-17@users.noreply.github.com>
2025-08-29 12:40:24 -0400

    about nav

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Navigation } from "../components/nav";
 import { Github, Linkedin, Mail, MapPin } from "lucide-react";
 
 export const metadata = {
