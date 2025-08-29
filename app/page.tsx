73652f8a024a82f14b2ef5cabedca214e5e1505a
Sachet-17 <90276515+Sachet-17@users.noreply.github.com>
2025-08-29 17:18:19 -0400

    hp

--- app/page.tsx
+++ app/page.tsx
@@ -39,8 +39,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I’m a Computer Engineer with a love for AI/ML, always chasing ideas that can turn into something useful in the real world. 
-          I enjoy building projects that make a difference.{" "}
+          AI/ML Engineer | Software Engineer | Tech Enthusiast - Actively seeking full time opportunites{" "}
         </h2>
       </div>
     </div>
